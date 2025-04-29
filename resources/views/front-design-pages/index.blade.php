9a9dd8e7d347b32f61b25a30a66b72c5faa0fcaf
ahmdsydysf <a.sayed.xc@gmail.com>
2025-04-29 10:31:52 +0400

    gg

--- resources/views/front-design-pages/index.blade.php
+++ resources/views/front-design-pages/index.blade.php
@@ -7,13 +7,13 @@
 
     <!-- SLIDER AREA START (slider-3) -->
 
-    <div class="ltn__slider-area ltn__slider-3  section-bg-1">
-        <div class="ltn__slide-one-active slick-slide-arrow-1 slick-slide-dots-1">
+    <div class="ltn__slider-area ltn__slider-3 section-bg-1" >
+        <div class="ltn__slide-one-active slick-slide-arrow-1 slick-slide-dots-1" style="height: 100%;">
             <!-- ltn__slide-item -->
             @isset($banners)
                 @foreach ($banners as $banner)
                     <div class="ltn__slide-item ltn__slide-item-2 ltn__slide-item-3 ltn__slide-item-3-normal"
-                        style="background-image: url('{{ asset('uploads/sliders/' . $banner->image) }}'); background-size: cover; background-position: center 10%;"
+                        style="background-image: url('{{ asset('uploads/sliders/' . $banner->image) }}'); background-size: cover; background-position: center 10%; height: 100%;"
                         tabindex="-1">
                         <div class="ltn__slide-item-inner">
                             <div class="container">
