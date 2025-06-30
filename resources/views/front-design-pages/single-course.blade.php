c335989a67f5c863618e36daf55f489e414895ae
ahmdsydysf <a.sayed.xc@gmail.com>
2025-06-30 15:47:23 +0400

    65 done

--- resources/views/front-design-pages/single-course.blade.php
+++ resources/views/front-design-pages/single-course.blade.php
@@ -707,7 +707,9 @@
                                           <i class="fab fa-facebook-f"></i>
                                       </a>
                                       <a href="#" class="social-btn twitter-btn" title="Share on Twitter">
-                                          <i class="fab fa-twitter"></i>
+                                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width: 1em; height: 1em; vertical-align: middle;">
+                                              <path fill="currentColor" d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/>
+                                          </svg>
                                       </a>
                                       <a href="#" class="social-btn linkedin-btn" title="Share on LinkedIn">
                                           <i class="fab fa-linkedin-in"></i>
