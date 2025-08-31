1c9c93d8f5ea4e24415d9052eb6913e8147c5eb8
sabreen mohammed <sabreen@wedomore.com>
2025-08-31 17:42:07 +0300

    fix category breadcramb

--- resources/views/front-design-pages/single-course.blade.php
+++ resources/views/front-design-pages/single-course.blade.php
@@ -542,7 +542,7 @@
                         @if($course->subCategory && $course->subCategory->courseCategory)
                             <li class="breadcrumb-item">
                                 <i class="fas fa-chevron-right breadcrumb-arrow"></i>
-                                <a href="{{ route('course-search', ['category_id' => $course->subCategory->courseCategory->id]) }}" class="breadcrumb-link">
+                                <a href="{{ route('category.show', ['id' => $course->subCategory->courseCategory->id]) }}" class="breadcrumb-link">
                                     {{ $course->subCategory->courseCategory->category_en_name }}
                                 </a>
                             </li>
