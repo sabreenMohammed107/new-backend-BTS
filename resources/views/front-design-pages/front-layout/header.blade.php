c60ba2306c7f52d70fe51b515c63ffdd7053f2aa
ahmdsydysf <a.sayed.xc@gmail.com>
2025-09-07 15:11:34 +0400

    change font type

--- resources/views/front-design-pages/front-layout/header.blade.php
+++ resources/views/front-design-pages/front-layout/header.blade.php
@@ -9,7 +9,9 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Marhey:wght@300..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <!-- Place favicon.png in the root directory -->
   <link rel="shortcut icon" href="{{ asset('front-assets/img/icons/ico.png') }}" type="image/x-icon" />
   <!-- Font Icons css -->
@@ -25,6 +27,7 @@
 <link href="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/css/tom-select.css" rel="stylesheet">
 <!-- TomSelect JS -->
 <script src="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/js/tom-select.complete.min.js"></script>
+
 
 <!-- تفعيل Tom Select -->
 <script>
